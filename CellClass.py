afbdbd626c5aa439c3ba060f38a43d04065ad761
Bailz <34628586+BailzPC@users.noreply.github.com>
2021-08-18 09:29:39 +0800

    Added pathfinding numbers
    Click 5 to update, does not draw over yet, still overlaps

--- CellClass.py
+++ CellClass.py
@@ -1,4 +1,7 @@
 import pygame
+# pygame.font.init()
+# myfont = pygame.font.SysFont('Comic Sans MS', 30)
+
 
 class Cell:
     def __init__(self,x,y,cell_type):
@@ -6,13 +9,22 @@ class Cell:
         self.y = y
         self.x = x
         self.cell_type = None
+
+    def calcpath(self, dist, grid):
+        if self.cell_type != "Button" and self.dist != None and self.dist <= (dist + 1):
+            return
         
-    def calcpath(self,dist):
-        if not self.dist:
+        if self.dist == None or dist < self.dist:
             self.dist = dist+1
-        elif self.dist > (dist+1):
-            self.dist = dist+1
-    
+
+        for xoff, yoff in ((-1,0), (0,-1), (1,0), (0,1)):
+            try:
+                neighbour = grid[self.y+yoff][self.x+xoff]
+                if neighbour.cell_type in [None, "Enemy", "Button"]:
+                    neighbour.calcpath(self.dist, grid)
+            except:
+                pass
+        
     def set_cell(self, celltype):
         self.cell_type = celltype
         print(celltype)
@@ -22,19 +34,24 @@ class Cell:
 
         if self.cell_type == "None":
             pygame.draw.rect(screen, (0,100,0), [self.x*self.cellsize+3,self.y*self.cellsize+3,self.cellsize-6,self.cellsize-6])
+#             textsurface = myfont.render(str(self.dist), False, (0, 0, 0))
+#             screen.blit(textsurface, (self.x*cellsize+6, self.y*cellsize+30))
             
         elif self.cell_type == "Block":
             pygame.draw.rect(screen, (120,120,120), [self.x*self.cellsize+3,self.y*self.cellsize+3,self.cellsize-6,self.cellsize-6])
             
+            
         elif self.cell_type == "Enemy":
             pygame.draw.rect(screen, (255,0,0), [self.x*self.cellsize+3,self.y*self.cellsize+3,self.cellsize-6,self.cellsize-6])
+        
         
         elif self.cell_type == "Void":
             pygame.draw.rect(screen, (50,50,200), [self.x*self.cellsize+3,self.y*self.cellsize+3,self.cellsize-6,self.cellsize-6])
             
+            
         elif self.cell_type == "Button":
             pygame.draw.rect(screen, (161,113,136), [self.x*self.cellsize+3,self.y*self.cellsize+3,self.cellsize-6,self.cellsize-6])
-
+            
             
         else:
             pass
--- TDPuzzle.py
+++ TDPuzzle.py
@@ -1,32 +1,23 @@
 import pygame
-import time
 from CellClass import Cell
 pygame.init()
 
 cellsize = 100
 grid = []
-rows = 12
-cols = 12
+rows = 10
+cols = 10
 block_selected = "Block"
 cell_type_clicked = "None"
 
 red = (255,0,0)
 white = (255,255,255)
+bg_colour = (0,100,0)
+button_colour = (161,113,136)
 
 width = cols*cellsize
 height = rows*cellsize
 screen = pygame.display.set_mode([width, height])
-screen.fill((0,100,0))
-
-clock = pygame.time.Clock()
-timerfont = pygame.font.SysFont(None, 200)
-
-counter = 4.0
-counternumber = counter
-
-timertext = timerfont.render(str(round(counter,1)), False, (0, 150, 200))
-timer_event = pygame.USEREVENT+1
-pygame.time.set_timer(timer_event, 100)
+screen.fill(bg_colour)
 
 game_started = True
 
@@ -34,9 +25,10 @@ for row in range(rows):
     for col in range(cols):
         rect_x = cellsize * col
         rect_y = cellsize * row
-#         print(rect_x)
 
         pygame.draw.rect(screen, (255,255,255), [rect_x,rect_y,cellsize,cellsize], 5)
+
+#append cells into grid
 
 for row in range(rows):
     grid.append([])
@@ -44,34 +36,36 @@ for row in range(rows):
         cell = Cell(col,row,None)
         grid[row].append(cell)
 
-grid[0][cols-1].cell_type = "Button"
-top_right = grid[0][cols-1].cell_type
+#changes where button is, button is end goal and timer
+
+button = grid[3][cols-5]
+button.cell_type = "Button"
+button.dist = 0
+button_x = button.x
+button_y = button.y
+
+clock = pygame.time.Clock()
+
+timer_started = False
+game_speed = 1000
+
+counter, text = 30, '30'.rjust(3)
+font = pygame.font.SysFont('Consolas', 36)
 
 while game_started:
-    
-    clock.tick(300)
-    
     for event in pygame.event.get():
-        print(event)
+        
+        #Timer logic, counts down every second
+        if event.type == pygame.USEREVENT: 
+            counter -= 1
+            text = str(counter).rjust(3) if counter > 0 else 'Win!'
+            
         if event.type == pygame.QUIT:
             game_started = False
         
-        if event.type == pygame.ACTIVEEVENT:
-            if event.gain == 1 and event.state == 6:
-                counter = counternumber
-                print("1")
-        elif event.type == timer_event:
-            counter -= 0.1
-            timertext = timerfont.render(str(round(counter,1)), True, (0, 150, 200))
-            if counter == 0:
-                pygame.time.set_timer(timer_event, 0)
-                print("2")
-                break
-            
-        pygame.display.flip()
-        pygame.display.update()
+        #Cycles through blocks to place using number keys
         
-        if event.type == pygame.KEYDOWN:
+        elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_1:
                 block_selected = "Block"
                 print(f"Selected: {block_selected}")
@@ -81,39 +75,88 @@ while game_started:
             elif event.key == pygame.K_3:
                 block_selected = "None"
                 print(f"Selected: {block_selected}")
-            
             elif event.key == pygame.K_0:
                 block_selected = "Void"
                 print(f"Selected: {block_selected}")
+                
+            #Recalculates all the pathfinding numbers
+            elif event.key == pygame.K_5:
+                for col in grid:
+                    for cell in col:
+                        if cell.cell_type != "Button":
+                            cell.dist = None
+                            
+                button.calcpath(button.dist, grid)
+                
+                for col in grid:
+                    for cell in col:
+                        cell.cell_colour(cellsize, screen)
+                        screen.blit(font.render(str(cell.dist), True, (0, 0, 0)), (cell.x*cellsize+6, cell.y*cellsize+32))
         
-        elif event.type == pygame.MOUSEBUTTONDOWN:
+        #If in the planning phase, places block by setting
+        #the tile you clicked on to block_selected, unless void or button cell
+        
+        elif event.type == pygame.MOUSEBUTTONDOWN and not timer_started:
             click_x = pygame.mouse.get_pos()[0]
             click_y = pygame.mouse.get_pos()[1]
-#            print(pygame.mouse.get_pos())
-#            print(click_x)
             col_clicked = click_x // cellsize
             row_clicked = click_y // cellsize
-#            print(f"row: {row_clicked} col: {col_clicked}")
-            
             cell_type_clicked = grid[row_clicked][col_clicked].cell_type
-#            print(f"Clicked: {cell_type_clicked}")
+            
+            
+            print(f"distance: {grid[row_clicked][col_clicked].dist}")
             
             if cell_type_clicked == "Void":
                 print("You cannot place there")
-            
-            elif cell_type_clicked == "Button":
-                pass
-                
+#             
+#             elif cell_type_clicked == "Button":
+#                 pygame.time.set_timer(pygame.USEREVENT, game_speed)
+#                 if not timer_started:
+#                     timer_started = True
+#                     print("The game has begun!")
+#                     
             else:
                 grid[row_clicked][col_clicked].cell_type = block_selected
+        
+        
+        #Cycle through timer speeds clicking the button after game started
                 
-            for col in grid:
-                    for cell in col:
-                        cell.cell_colour(cellsize, screen)
+        elif event.type == pygame.MOUSEBUTTONDOWN and timer_started:
+            click_x = pygame.mouse.get_pos()[0]
+            click_y = pygame.mouse.get_pos()[1]
+            col_clicked = click_x // cellsize
+            row_clicked = click_y // cellsize
+
+            cell_type_clicked = grid[row_clicked][col_clicked].cell_type
+            
+            if cell_type_clicked == "Button":
+                if timer_started == True:
+                    if game_speed == 1000:
+                        game_speed = 500
+                        print("2x")
+                    elif game_speed == 500:
+                        game_speed = 250
+                        print("4x")
+                    elif game_speed == 250:
+                        game_speed = 1000
+                        print("1x")
+                    pygame.time.set_timer(pygame.USEREVENT, game_speed)
                     
+                else:
+                    print("Timer has been started!")
+                
         else:
             pass
-        
-        pygame.display.update()
-        
+    
+    #updates the screen every loop, cells colour themselves
+    #
+    
+    for col in grid:
+        for cell in col:
+            cell.cell_colour(cellsize, screen)
+    screen.blit(font.render(text, True, (0, 0, 0)), (button_x*cellsize+6, button_y*cellsize+32))
+    pygame.display.flip()
+    pygame.display.update()
+    clock.tick(60)
+    
 pygame.quit()
